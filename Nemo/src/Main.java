9fc95077573cb8308b1de7657e1863104af56149
wnsgu <wnsgu@172.17.230.162>
2022-11-10 21:27:12 +0900

    첫번쨰 업로드

--- Nemo/src/Main.java
+++ Nemo/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("팀장 박준형입니다^w^");
+		System.out.println("팀장 박준형입니다 ^w^");
 	}
 
 }
