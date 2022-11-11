0126a9e3f80e6a9a02ea5fe2f6532f41457f8d14
82105 <82105@DESKTOP-VGNKQLM>
2022-11-11 20:11:08 +0900

    첫번째 업로드

--- Nemo/src/Main.java
+++ Nemo/src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("1234");
-		System.out.println("안녕 난 곱등이");
-		System.out.println("좀 되라 ");
-		System.out.println("좀 되라 ");
 		System.out.println("kang test");
 		System.out.println("seo tset");
 		System.out.println("팀장독대나무다리어카센터미널뛰기러기차");
+		System.out.println("kyh test");
 	}
 }
