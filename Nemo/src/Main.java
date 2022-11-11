4cae89d433aa561d4bffcf58584092ef203212a6
zerrissene <zerrissene@localhost>
2022-11-11 18:13:55 +0900

    kang test

--- Nemo/src/Main.java
+++ Nemo/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("1234");
 		System.out.println("안녕 난 곱등이");
-		System.out.println("좀 되라 ㅅㅂ");
-		System.out.println("좀 되라 ㅅㅂ");
-		System.out.println("좀 되라 ㅅㅂ");
+		System.out.println("좀 되라 ");
+		System.out.println("좀 되라 ");
+		System.out.println("kang test");
 		System.out.println("seo tset");
 	}
 }
