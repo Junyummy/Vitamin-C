48aa3a886f55714d2a09e0fdba82865923f88628
82105 <82105@DESKTOP-VGNKQLM>
2022-11-15 10:15:08 +0900

    MainFrame

--- Nemo/src/Main.java
+++ Nemo/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("kang test");
-		System.out.println("seo tset");
-		System.out.println("팀장독대나무다리어카센터미널뛰기러기차");
-		System.out.println("kyh test");
+		new MainFrame();
 	}
 }
