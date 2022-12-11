9d16ebdb8bdba4bda66e08a8e1e094a8dc1ad80e
82105 <82105@DESKTOP-VGNKQLM>
2022-12-11 19:08:02 +0900

    다시 올림

---
+++ Nemo/src/MainBoard15X15.java
@@ -0,0 +1,253 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MainBoard15X15 extends Canvas
+	implements MouseListener, MouseMotionListener
+{	
+	MainPanel15X15 parent; //Nemonemo 클래스의 객체를 저장
+	OtherFrame.JPanelTest win;
+	Nemonemo nemo;
+	Nemonemo1 nemo1;
+	Nemonemo2 nemo2;
+	Nemonemo3 nemo3;
+	Nemonemo4 nemo4;
+	Nemonemo5 nemo5;
+	Nemonemo6 nemo6;
+	Nemonemo7 nemo7;
+	Nemonemo8 nemo8;
+
+	
+	boolean drag = false; //마우스 드래그 상태인지 여부
+	int startX, startY; //마우스 드래그를 시작한 좌표
+	int endX, endY; //마우스 드래그를 끝마친 좌표
+	private Dimension dim;
+	Image offScr; //더블버퍼링을 위한 가상 화면
+	Graphics offG;
+
+
+	public MainBoard15X15(MainPanel15X15 mainp2) //Nemonemo 클래스의 객체를 보관하고 리스너를 선언
+	{
+		this.parent = mainp2; //Nemonemo 클래스의 객체를 보관
+		nemo = new Nemonemo(win);
+		nemo1 = new Nemonemo1(win);
+		nemo2 = new Nemonemo2(win);
+		nemo3 = new Nemonemo3(win);
+		nemo4 = new Nemonemo4(win);
+		nemo5 = new Nemonemo5(win);
+		nemo6 = new Nemonemo6(win);
+		nemo7 = new Nemonemo7(win);
+		nemo8 = new Nemonemo8(win);
+		
+	
+		this.addMouseListener(this); //마우스 사용을 위한 리스너 선언
+		this.addMouseMotionListener(this);
+
+	}
+
+	private void addMouseMotionListener(MainBoard15X15 mainBoard15X15) {
+	// TODO Auto-generated method stub
+	
+	}
+
+	private void addMouseListener(MainBoard15X15 mainBoard15X15) {
+	// TODO Auto-generated method stub
+	
+	}
+
+	public void initBufferd() {
+		dim = getSize();
+		setBackground(Color.white);
+		offScr = createImage(dim.width,dim.height);
+		offG = offScr.getGraphics();
+	}
+
+	public void paint(Graphics g) {
+		initBufferd();
+		offG.clearRect(0, 0, dim.width, dim.height);
+		offG.setColor(Color.black);
+		offG.setFont(new Font("SansSerif", Font.BOLD, 14));
+		offG.drawString("15X15", 85,20);
+	
+		if(nemo.getendFlag()) {
+			offG.setColor(Color.black);
+			offG.fillRect(10, 30, 60, 60);
+		}
+	
+		else {
+			offG.setColor(Color.blue);
+			offG.fillRect(10,30,60,60);
+		}
+	
+		if(nemo1.getendFlag()) {
+			offG.setColor(Color.green);
+			offG.fillRect(70, 30, 60, 60);
+		}
+		else {
+			offG.setColor(Color.red);
+			offG.fillRect(70,30,60,60);
+		}
+	
+		if(nemo2.getendFlag()) {
+			offG.setColor(Color.yellow);
+			offG.fillRect(130, 30, 60, 60);
+		}
+		else {
+			offG.setColor(Color.orange);
+			offG.fillRect(130,30,60,60);
+		}	
+	
+		if(nemo3.getendFlag()) {
+		offG.setColor(Color.pink);
+		offG.fillRect(10, 90, 60, 60);
+	}
+	else {
+		offG.setColor(Color.gray);
+		offG.fillRect(10,90,60,60);
+	}
+	
+	if(nemo4.getendFlag()) {
+		offG.setColor(Color.cyan);
+		offG.fillRect(70, 30, 60, 60);
+	}
+	else {
+		offG.setColor(Color.black);
+		offG.fillRect(70,90,60,60);
+	}
+	
+	if(nemo5.getendFlag()) {
+		offG.setColor(Color.red);
+		offG.fillRect(130, 30, 60, 60);
+	}
+	else {
+		offG.setColor(Color.cyan);
+		offG.fillRect(130,90,60,60);
+	}
+	
+	if(nemo6.getendFlag()) {
+		offG.setColor(Color.blue);
+		offG.fillRect(10, 150, 60, 60);
+	}
+	else {
+		offG.setColor(Color.yellow);
+		offG.fillRect(10,150,60,60);
+	}
+	
+	if(nemo6.getendFlag()) {
+		offG.setColor(Color.gray);
+		offG.fillRect(70, 150, 60, 60);
+	}
+	else {
+		offG.setColor(Color.green);
+		offG.fillRect(70, 150, 60, 60);
+	}
+	
+	if(nemo6.getendFlag()) {
+		offG.setColor(Color.orange);
+		offG.fillRect(130, 150, 60, 60);
+	}
+	else {
+		offG.setColor(Color.pink);
+		offG.fillRect(130, 150, 60, 60);
+	}
+	offG.setColor(Color.black);
+	offG.fillRect(400, 400, 40, 40);
+	g.drawImage(offScr, 0, 0, null); //가상 화면을 실제 화면으로 복사
+}
+
+public MainBoard15X15(ActionListener actionListener) {
+	// TODO Auto-generated constructor stub
+}
+
+@Override
+public void mouseDragged(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseMoved(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	int x = e.getX();
+	int y = e.getY();
+	System.out.println(x + " "+ y);
+	if((e.getModifiers() & InputEvent.BUTTON3_MASK)!=0) 
+		//마우스 오른쪽 버튼
+	{
+		
+	}
+	else //마우스 왼쪽 버튼 
+	{
+		if((x>29 && x<91) && (y>29 && y<91)) {
+			parent.win.change("nemo");
+		}
+		if((x>89 && x<151) && (y>29 && y<91)) {
+			parent.win.change("nemo1");
+		}
+		if((x>149 && x<211) && (y>29 && y<91)) {
+			parent.win.change("nemo2");
+		}
+		if((x>29 && x<91) && (y>89 && y<151)) {
+			parent.win.change("nemo3");
+		}
+		if((x>89 && x<151) && (y>89 && y<151)) {
+			parent.win.change("nemo4");
+		}
+		if((x>149 && x<211) && (y>89 && y<151)) {
+			parent.win.change("nemo5");
+		}
+		if((x>29 && x<91) && (y>159 && y<211)) {
+			parent.win.change("nemo6");
+		}
+		if((x>89 && x<151) && (y>159 && y<211)) {
+			parent.win.change("nemo7");
+		}
+		if((x>149 && x<211) && (y>159 && y<211)) {
+			parent.win.change("nemo8");
+		}
+		if((x>399 && x<441) && (y>399 && y<441)) {
+			parent.win.change("mainp");
+		}
+	}
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+}
---
+++ Nemo/src/MainPanel15X15.java
@@ -0,0 +1,70 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import javazoom.jl.player.Player;
+
+public class MainPanel15X15 extends JPanel{	
+	OtherFrame.JPanelTest win;
+	JPanel Mainp2;
+	
+	MainBoard15X15 board2;
+	
+	public MainPanel15X15(OtherFrame.JPanelTest win){
+		setLayout(null);
+		this.win = win;
+
+		Mainp2 = new JPanel();
+		Mainp2.setLayout(null); //null 레이아웃으로 설정			
+		
+		//board 생성
+		board2 = new MainBoard15X15(this);
+		this.add(board2);
+		board2.setFont(new Font("SansSerif", Font.BOLD, 14));
+		board2.setBounds(120, 120, 601, 601);
+		bgplay();
+		JButton btn = new JButton("10X10");
+	    btn.setSize(110, 20);
+        btn.setLocation(7, 391);
+        add(btn);
+        btn.addActionListener(new MyActionListener());
+        bgplay();
+	}
+	class MyActionListener implements ActionListener { // 버튼 키 눌리면 패널 1번 호출
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            win.change("mainp");
+        }
+    }
+	
+	
+
+	private void bgplay() {
+		Player jlPlayer = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream("resources/Empty.mp3");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            jlPlayer = new Player(bufferedInputStream);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        final Player player = jlPlayer;
+        new Thread() {
+            public void run() {
+                try {
+                	player.play();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }.start();
+    }
+	
+}
